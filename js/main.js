0f891b5194eefafd8b5b38e873cba0c488fe9431
Andrew Rogers <abennobashi@gmail.com>
2011-08-20 22:27:05 -0700

    adding manipulateable string array

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 // JavaScript Document
 
-alert("test");
-alert("sup");
+var elementIds = new Array("pagelet_welcome_box", "pinnedNav", "appsNav", "groupsNav", "pagesNav");
 
 window.onload = function() {
 	$("pagelet_welcome_box").hide();
+}
+
+function hideDivElement()
+{
+	this.hide();
 }
