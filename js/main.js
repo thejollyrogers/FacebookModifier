c03f897e88351b33a306526f2cf6f19e4bddcdc6
Andrew Rogers <abennobashi@gmail.com>
2011-08-21 15:42:16 -0700

    adding "class" array for targeting to classes. some refactoring. couldnt work much, working on other websites

--- js/main.js
+++ js/main.js
@@ -1,41 +1,59 @@
 // JavaScript Document
 
-var elementIds = new Array("pagelet_welcome_box", "pinnedNav", "appsNav", "groupsNav", "pagesNav");
+// for elements with IDs to target
+var elementIds = new Array("pagelet_welcome_box", "pinnedNav", "appsNav", "groupsNav", "pagesNav", "pagelet_eventbox");
+
+// for elements with classes to target
+var elementClasses = new Array("ego_section");
+
+// current element we're interacting with right now
 var currentElement;
+
+// TODO remove this
 var showingX;
 
 window.onload = function() {
 	for(var i = 0; i < elementIds.length; i++){
-			$(elementIds[i]).observe("mouseover", addX);
-			$(elementIds[i]).observe("mouseout", removeX);
+		var element = $(elementIds[i]);
+		element.observe("mouseover", showXBox);
+		element.observe("mouseout", hideXBox);
+	}
+	
+	// is there a better way to handel these?
+	for(var i = 0; i < elementClasses.length; i++){
+		var classArray = $$(elementClasses[i]);
+		for(var j = 0; j < elementClasses.length; j++)
+			classArray[j].observe("mousemove", addX);
+			
 	}
 }
 
-function addX(){
+function addXBoxToElement(element){
 	if(!showingX)
 	{
 		showingX = true;
 		currentElement = this;
 		var xElement = $(document.createElement("div"));
 		xElement.addClassName("xbox");
-		xElement.observe("mouseover", addMouseOver);
-		xElement.observe("mouseout", removeMouseOver);
+		//xElement.observe("mouseover", addMouseOverClass);
+		//xElement.observe("mouseout", removeMouseOverClass);
 		xElement.observe("click", hideDivParentElement);
 		xElement.innerHTML = ":)";
-		this.insertBefore(xElement, this.firstChilds);
+		this.insertBefore(xElement, this.firstChild);
 	}
 }
 
 function removeX(){
 	this.firstChild.remove();
 	showingX = false;	
+	currentElement = null;
 }
 
-function removeMouseOver(){
+function removeMouseOverClass(){
 	this.removeClassName("mouseoverxbox");
 }
 
-function addMouseOver(){
+function addMouseOverClass(){
 	this.addClassName("mouseoverxbox");	
 }
 
@@ -43,9 +61,12 @@ function hideDivElement()
 {
 	showingX = false;
 	this.hide();
+	if(currentElement != null)
+		currentElement.hide();
 }
 
 function hideDivParentElement(){
-	currentElement.hide();
+	if(currentElement != null)
+		currentElement.hide();
 	showingX = false;
 }
