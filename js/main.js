86b35e1bb8bfad063ae6eba2c68f59171d04183a
pleland17 <pleland17@gmail.com>
2011-08-20 23:08:25 -0700

    added a bunch of stuff related to creating an x image

--- js/main.js
+++ js/main.js
@@ -1,14 +1,37 @@
 // JavaScript Document
 
 var elementIds = new Array("pagelet_welcome_box", "pinnedNav", "appsNav", "groupsNav", "pagesNav");
+var currentElement;
 
 window.onload = function() {
 	for(var i = 0; i < elementIds.length; i++){
-		$(elementIds[i]).observe("mousemove", hideDivElement);	
+		$(elementIds[i]).observe("mousemove", addX);	
 	}
+}
+
+function addX(){
+	currentElement = this;
+	var xElement = $(document.createElement("div"));
+	xElement.addClassName("xbox");
+	xElement.observe("mousemove", addMouseOver);
+	xElement.observe("mouseout", removeMouseOver);
+	xElement.observe("click", hideDivParentElement);
+	this.appendChild(xElement);
+}
+
+function removeMouseOver(){
+	this.removeClassName("mouseoverxbox");	
+}
+
+function addMouseOver(){
+	this.addClassName("mouseoverxbox");	
 }
 
 function hideDivElement()
 {
 	this.hide();
+}
+
+function hideDivParentElement(){
+	currentElement.hide();
 }
